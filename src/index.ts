abea1b51f0dc2f0b81342d07d84f68041d199b45
An1by <anibycor@gmail.com>
2025-04-27 18:48:31 +0300

    fix: cors appt2

--- src/index.ts
+++ src/index.ts
@@ -25,7 +25,8 @@ Bun.serve({
                 );
                 const response = Response.json(await goalResponse.json());
                 response.headers.set('Access-Control-Allow-Origin', '*');
-                response.headers.set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+                response.headers.set('Access-Control-Allow-Methods', 'OPTIONS, GET');
+                response.headers.set('Access-Control-Allow-Headers', 'Content-Type, Authorization');
                 return response;
             } catch (error) {
                 return new Response("Invalid Authorization header", { status: 400 });
