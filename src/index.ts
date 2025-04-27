f4169e84f4dcb479b6879fa550a2b0e6806569e0
An1by <anibycor@gmail.com>
2025-04-27 19:49:04 +0300

    feat: /da/oauth feat: /da/user/widget

--- src/index.ts
+++ src/index.ts
@@ -12,13 +12,28 @@ Bun.serve({
             return Response.redirect("https://aniby.net/", 301);
         },
 
-
-        "/da/goal-info": async (req, server,) => {
+        "/da/oauth": async (req) => {
             const {searchParams} = new URL(req.url)
-            if (!searchParams.has("token")) {
+            if (!searchParams.has("scope")) {
+                return new Response("Scopes not found", {status: 401});
+            }
+            const scope = searchParams.get("scope")!;
+
+            const url = `https://www.donationalerts.com/oauth/authorize?client_id=${Bun.env.DA_APP_ID}
+                &redirect_uri=${Bun.env.HOST}/oauth/confirm
+                &response_type=token
+                &scope=${scope}`;
+            return Response.redirect(url, 301);
+        },
+        "/da/oauth/confirm": async (req) => {
+            return new Response(Bun.file("./src/pages/da/auth.html"));
+        },
+        "/da/goal/info": async (req,) => {
+            const {searchParams} = new URL(req.url)
+            if (!searchParams.has("accessToken")) {
                 return new Response("Authorization token not found", {status: 401});
             }
-            const token = searchParams.get("token")!;
+            const token = searchParams.get("accessToken")!;
 
             if (!searchParams.has("id")) {
                 return new Response("Goal id not found", {status: 401});
@@ -37,7 +52,30 @@ Bun.serve({
                 const result = await goalResponse.json();
                 return new Response(JSON.stringify(result), responseInit);
             } catch (error) {
-                return new Response("Invalid Authorization header", {status: 400});
+                return new Response("DonationAlerts Error", {status: 400});
+            }
+        },
+
+        "/da/user/widget": async (req) => {
+            const {searchParams} = new URL(req.url)
+            if (!searchParams.has("accessToken")) {
+                return new Response("Authorization token not found", {status: 401});
+            }
+            const token = searchParams.get("accessToken")!;
+
+            try {
+                const goalResponse = await fetch(
+                    `https://www.donationalerts.com/api/v1/user/widget`,
+                    {
+                        headers: {
+                            Authorization: `Bearer ${token}`,
+                        }
+                    }
+                );
+                const result = await goalResponse.json();
+                return new Response(JSON.stringify(result), responseInit);
+            } catch (error) {
+                return new Response("DonationAlerts Error", {status: 400});
             }
         },
 
